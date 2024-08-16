b9ee391bf0bfac5b155397f828eee89c1639b09f
jesse <jesse@gmail.com>
2024-08-16 12:46:49 +0530

    feat: routing

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -4,21 +4,22 @@ export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
-  if(error.status == 404){
-    return <>
-      <div>
-        <h3>Ohh!!</h3>
-        <p>We are not able to find the page for the given Url</p>
-        <Link to='/'>Navigate Home </Link>
-      </div>
-    </>
-  }
-  return (
+  if (error.status == 404) {
+    return (
       <>
         <div>
+          <h3>Ohh!!</h3>
+          <p>We are not able to find the page for the given Url</p>
+          <Link to="/">Navigate Home </Link>
+        </div>
+      </>
+    );
+  }
+  return (
+    <>
+      <div>
         <h3>something went wrong</h3>
       </div>
-        </>
-  )
-}
+    </>
+  );
 }
