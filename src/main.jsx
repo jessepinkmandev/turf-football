dca4e299bca49e8564f81a59928cdd476b0ae839
jesse <jesse@gmail.com>
2024-08-16 12:40:42 +0530

    feat: routing

--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           <Route errorElement={<ErrorPage />} path="/" element={<Root />}>
             <Route path="signup" element={<SignUp />} />
             <Route path="signin" element={<SignIn />} />
-            <Route path="/*" element={<Root />} />
+            <Route path="/*" element={<ErrorPage />} />
             <Route
               path="more"
               element={
