22f9a50792aab77e4f4cc32c7ddaa3e1df76fc39
jesse <jesse@gmail.com>
2024-08-16 13:12:46 +0530

    feat: routing

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -1,24 +1,14 @@
-import { Link, useRouteError } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-export default function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
+export default function ErrorPage({ error }) {
+  console.log("error");
 
-  if (error.status == 404) {
-    return (
-      <>
-        <div>
-          <h3>Ohh!!</h3>
-          <p>We are not able to find the page for the given Url</p>
-          <Link to="/">Navigate Home </Link>
-        </div>
-      </>
-    );
-  }
   return (
     <>
       <div>
-        <h3>something went wrong</h3>
+        <h3>Ohh!!</h3>
+        <p>We are not able to find the page for the given Url</p>
+        <Link to="/">Navigate Home </Link>
       </div>
     </>
   );
